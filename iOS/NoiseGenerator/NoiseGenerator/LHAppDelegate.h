8a86744cd2207aa39eed354aaef3333110d8c5c7
don1ck <vprivalov@ruswizards.com>
2014-07-11 23:14:55 +0300

    Empty iOS Project created

---
+++ iOS/NoiseGenerator/NoiseGenerator/LHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LHAppDelegate.h
+//  NoiseGenerator
+//
+//  Created by Victor on 11.07.14.
+//  Copyright (c) 2014 Lone House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/NoiseGenerator/NoiseGenerator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NoiseGenerator
+//
+//  Created by Victor on 11.07.14.
+//  Copyright (c) 2014 Lone House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([LHAppDelegate class]));
+	}
+}
